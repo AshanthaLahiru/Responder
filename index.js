63ffca022f1ab9a949367ee87b4f13ba6db93b43
Ashantha Lahiru <lashantha@virtusa.com>
2018-02-15 16:45:15 +0530

    Modified github comment api call

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 module.exports = (robot) => {
   robot.on('pull_request.opened', async context => {
 
+    context.log("*******CONTEXT***********");
+    context.log(context);
+    context.log("*******CONTEXT***********");
     let days = "{{days}}";
     const params = context.issue({body: 'We appreciate your contribution towards the project will get back to you within ' + days + '}'});
 
-    return context.github.pullRequests.createComment(params);
+    return context.github.issues.createComment(params);
   })
 }
