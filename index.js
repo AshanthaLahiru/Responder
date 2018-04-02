0aa6a8aa33196f10957a5e8faffe1638edf6cd5d
Ashantha Lahiru <ashantha.lahiru@gmail.com>
2018-04-02 22:44:52 +0530

    Added reminder

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-module.exports = (robot) => {
-  robot.on('issues.opened', async context => {
+const metadata = require('probot-metadata')
+const moment = require('moment')
+const createScheduler = require('probot-scheduler')
 
+module.exports = (robot) => {
+  createScheduler(robot, { interval: 1 * 30 * 1000 })
+  robot.on(['pull_request.opened', 'issues.opened'], async context => {
     const contextSummary = context.issue()
     let contributors = (await context.github.repos.getContributors(contextSummary)).data
-    
+
     let contributorsIds = []
     if (contributors && Array.isArray(contributors)) {
       contributors.forEach(contributor => {
@@ -20,7 +24,7 @@ module.exports = (robot) => {
 
     let issueParams = context.issue({ state: 'all' })
     let issuesForRepo = (await context.github.issues.getForRepo(issueParams)).data
-    
+
     if (issuesForRepo && Array.isArray(issuesForRepo)) {
       let firstResponseTimes = await Promise.all(issuesForRepo.map(async issue => {
         if (contextSummary.number !== issue.number) {
@@ -47,7 +51,7 @@ module.exports = (robot) => {
       console.log('>>>>>>>>firstResponseTimes')
       console.log(firstResponseTimes)
       console.log('<<<<<<<<firstResponseTimes')
-  
+
       let totalTime = 0
       let totalCount = 0
       for (let x = 0; x < firstResponseTimes.length; x++) {
@@ -56,15 +60,15 @@ module.exports = (robot) => {
           totalCount++
         }
       }
-  
+
       console.log('>>>>>>>>totalTime')
       console.log(totalTime)
       console.log('<<<<<<<<totalTime')
-  
+
       console.log('>>>>>>>>totalCount')
       console.log(totalCount)
       console.log('<<<<<<<<totalCount')
-  
+
       let averageResponseTime = totalTime / totalCount
       let formattedResponseTime
       if (!totalTime || totalTime === 0) {
@@ -76,7 +80,7 @@ module.exports = (robot) => {
       } else {
         formattedResponseTime = Math.ceil(averageResponseTime / (3600 * 24)) + ' days'
       }
-  
+
       let commentMessage
       if (formattedResponseTime === 'soon') {
         commentMessage = 'Thank you for openning an issue. We appreciate your contribution towards the project, someone will get back to you ' + formattedResponseTime + '. :hourglass: '
@@ -84,8 +88,83 @@ module.exports = (robot) => {
         commentMessage = 'Thank you for openning an issue. We appreciate your contribution towards the project, based on past issues someone will get back to you within ' + formattedResponseTime + '. :hourglass: '
       }
       const commentParams = context.issue({ body: commentMessage })
-  
-      return context.github.issues.createComment(commentParams)
+
+      await context.github.issues.createComment(commentParams)
+
+      const { labels } = context.payload.issue
+      labels.push('waiting for response')
+      let reminder = moment().add(formattedResponseTime.split(' ')[0], formattedResponseTime.split(' ')[1]).format()
+
+      const timeout = {
+        created: context.payload.issue.created_at,
+        due: reminder
+      }
+
+      await metadata(context).set('timeout', timeout)
+      await context.github.issues.addLabels(context.issue({ labels }))
     }
+  })
+
+  robot.on('schedule.repository', async context => {
+    // let config
+
+    // try {
+    //   config = await context.config('config.yml', defaults)
+    // } catch (err) {
+    //   config = defaults
+    // }
+
+    const { owner, repo } = context.repo()
+    const q = `label:"waiting for response" repo:${owner}/${repo}`
+
+    const resp = await context.github.search.issues({ q })
+    let lastLabels = []
+    await Promise.all(resp.data.items.map(async issue => {
+      issue = context.repo(issue)
+      const { owner, repo, number } = issue
+
+      const timeout = await metadata(context, issue).get('timeout')
+      const due = timeout.due
+
+      if (!due) {
+        // Malformed metadata, not much we can do
+        await context.github.issues.removeLabel({
+          owner,
+          repo,
+          number,
+          name: 'waiting for response'
+        })
+      } else if (moment(due) > moment()) {
+        const remainingPercentage = ((Date.parse(due) - new Date()) / (Date.parse(due) - Date.parse(timeout.created))) * 100
+
+        const dynamicLabel = `reponse timeout: ${Math.round(remainingPercentage)}% :hourglass_flowing_sand:`
+
+        const labels = issue.labels
+
+        const lastLabel = labels.find(label => {
+          return label.name.split(':')[0] === 'reponse timeout'
+        })
+        if (lastLabel) {
+          lastLabels.push(lastLabel)
+          const pos = labels.indexOf(lastLabel)
+          labels.splice(pos, 1)
+        }
+        labels.push(dynamicLabel)
+
+        await context.github.issues.edit({ owner, repo, number, labels, state: 'open' })
+      } else if (moment(due) <= moment()) {
+        await context.github.issues.removeLabel({ owner, repo, number, name: 'waiting for response' })
+      }
+    }))
+
+    await Promise.all(lastLabels.map(async label => {
+      const q = `label:"${label.name}" repo:${owner}/${repo}`
+      const resp = await context.github.search.issues({ q })
+
+      if (resp.data.items.length === 0) {
+        let name = label.name
+        context.github.issues.deleteLabel({ owner, repo, name })
+      }
+    }))
   })
 }
