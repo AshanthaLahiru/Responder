81554b0040c657569bb040fcd7cf4835ffff110a
Ashantha Lahiru <ashantha.lahiru@gmail.com>
2018-04-28 21:30:20 +0530

    Added owner validations

--- bin/responder.js
+++ bin/responder.js
@@ -34,106 +34,100 @@ module.exports = {
             console.log(contributorsIds)
             console.log('<<<<<<<<IDs')
 
-            let issueParams = context.issue({ state: 'all' })
-            let issuesForRepo = (await context.github.issues.getForRepo(issueParams)).data
+            if (!contributorsIds.includes(context.payload.sender.id)) {
+                let issueParams = context.issue({ state: 'all' })
+                let issuesForRepo = (await context.github.issues.getForRepo(issueParams)).data
 
-            issuesForRepo = issuesForRepo.slice(0, config.response.activities_count || 10)
+                issuesForRepo = issuesForRepo.slice(0, config.response.activities_count || 10)
 
-            if (issuesForRepo && Array.isArray(issuesForRepo)) {
-                let firstResponseTimes = await Promise.all(issuesForRepo.map(async issue => {
-                    if (contextSummary.number !== issue.number) {
-                        let commentsForRepoParams = context.issue({ number: issue.number })
-                        let commentsForIssue = (await context.github.issues.getComments(commentsForRepoParams)).data
-                        if (commentsForIssue.length === 0) {
-                            return -1
-                        } else {
-                            for (let x = 0; x < commentsForIssue.length; x++) {
-                                let comment = commentsForIssue[x]
-                                if (contributorsIds.includes(comment.user.id)) {
-                                    let firstResponseTime = 0
-                                    firstResponseTime = (Date.parse(comment.created_at) - Date.parse(issue.created_at)) / 1000
+                if (issuesForRepo && Array.isArray(issuesForRepo)) {
+                    let firstResponseTimes = await Promise.all(issuesForRepo.map(async issue => {
+                        if (contextSummary.number !== issue.number) {
+                            let commentsForRepoParams = context.issue({ number: issue.number })
+                            let commentsForIssue = (await context.github.issues.getComments(commentsForRepoParams)).data
+                            let firstRespondComment = commentsForIssue.find((comment) => {
+                                return contributorsIds.includes(comment.user.id)
+                            })
 
-                                    return firstResponseTime
-                                } else if (x === commentsForIssue.length - 1) {
-                                    return -1
-                                }
+                            if (firstRespondComment) {
+                                return (Date.parse(firstRespondComment.created_at) - Date.parse(issue.created_at)) / 1000
                             }
                         }
+                    })
+                    )
+                    console.log('>>>>>>>>firstResponseTimes')
+                    console.log(firstResponseTimes)
+                    console.log('<<<<<<<<firstResponseTimes')
+
+                    let totalTime = 0
+                    let totalCount = 0
+                    for (let x = 0; x < firstResponseTimes.length; x++) {
+                        if (firstResponseTimes[x]) {
+                            totalTime += firstResponseTimes[x]
+                            totalCount++
+                        }
                     }
-                })
-                )
-                console.log('>>>>>>>>firstResponseTimes')
-                console.log(firstResponseTimes)
-                console.log('<<<<<<<<firstResponseTimes')
 
-                let totalTime = 0
-                let totalCount = 0
-                for (let x = 0; x < firstResponseTimes.length; x++) {
-                    if (firstResponseTimes[x] && firstResponseTimes[x] !== -1) {
-                        totalTime += firstResponseTimes[x]
-                        totalCount++
+                    console.log('>>>>>>>>totalTime')
+                    console.log(totalTime)
+                    console.log('<<<<<<<<totalTime')
+
+                    console.log('>>>>>>>>totalCount')
+                    console.log(totalCount)
+                    console.log('<<<<<<<<totalCount')
+
+                    let averageResponseTime = totalTime / totalCount
+
+                    if (config.response.buffer_time) {
+                        averageResponseTime += moment.duration(Number(config.response.buffer_time.split(' ')[0]), config.response.buffer_time.split(' ')[1]).asSeconds()
                     }
-                }
 
-                console.log('>>>>>>>>totalTime')
-                console.log(totalTime)
-                console.log('<<<<<<<<totalTime')
-
-                console.log('>>>>>>>>totalCount')
-                console.log(totalCount)
-                console.log('<<<<<<<<totalCount')
-
-                let averageResponseTime = totalTime / totalCount
-
-                if (config.response.buffer_time) {
-                    averageResponseTime += moment.duration(Number(config.response.buffer_time.split(' ')[0]), config.response.buffer_time.split(' ')[1]).asSeconds()
-                }
-
-                let formattedResponseTime
-                if (!totalTime || totalTime === 0) {
-                    formattedResponseTime = 'soon'
-                } else if (averageResponseTime < 3600) {
-                    formattedResponseTime = Math.ceil(averageResponseTime / 60) + ' minutes'
-                } else if (averageResponseTime < 3600 * 24) {
-                    formattedResponseTime = Math.ceil(averageResponseTime / 3600) + ' hours'
-                } else {
-                    formattedResponseTime = Math.ceil(averageResponseTime / (3600 * 24)) + ' days'
-                }
-
-                let commentMessage
-                if (formattedResponseTime === 'soon') {
-                    commentMessage = config.response.reponse_message.soon_reponse
-                } else {
-                    commentMessage = `${config.response.reponse_message.time_response} ${formattedResponseTime}. :hourglass: <br/><br/>`
-                }
-
-                if (config.categorizer.available) {
-                    if (config.categorizer.guideComment) {
-                        commentMessage = `${commentMessage} ${config.categorizer.guide_comment} <br/><br/>`
+                    let formattedResponseTime
+                    if (!totalTime || totalTime === 0) {
+                        formattedResponseTime = 'soon'
+                    } else if (averageResponseTime < 3600) {
+                        formattedResponseTime = Math.ceil(averageResponseTime / 60) + ' minutes'
+                    } else if (averageResponseTime < 3600 * 24) {
+                        formattedResponseTime = Math.ceil(averageResponseTime / 3600) + ' hours'
                     } else {
-                        commentMessage = `${commentMessage} It would be helpful if you can categorize this ${eventType} under following categories. \n\n `
+                        formattedResponseTime = Math.ceil(averageResponseTime / (3600 * 24)) + ' days'
                     }
 
-                    for (let x = 0; x < config.categorizer.categories.length; x++) {
-                        commentMessage = commentMessage.concat('- `/cat-' + config.categorizer.categories[x].keyword + '-` : ' + config.categorizer.categories[x].label.description + '\n')
+                    let commentMessage
+                    if (formattedResponseTime === 'soon') {
+                        commentMessage = config.response.reponse_message.soon_reponse
+                    } else {
+                        commentMessage = `${config.response.reponse_message.time_response} ${formattedResponseTime}. :hourglass: <br/><br/>`
                     }
+
+                    if (config.categorizer.available) {
+                        if (config.categorizer.guideComment) {
+                            commentMessage = `${commentMessage} ${config.categorizer.guide_comment} <br/><br/>`
+                        } else {
+                            commentMessage = `${commentMessage} It would be helpful if you can categorize this ${eventType} under following categories. \n\n `
+                        }
+
+                        for (let x = 0; x < config.categorizer.categories.length; x++) {
+                            commentMessage = commentMessage.concat('- `/cat-' + config.categorizer.categories[x].keyword + '-` : ' + config.categorizer.categories[x].label.description + '\n')
+                        }
+                    }
+
+                    const commentParams = context.issue({ body: commentMessage })
+
+                    await context.github.issues.createComment(commentParams)
+
+                    const { labels } = context.payload.issue
+                    labels.push(config.reminder.label.waiting)
+                    let reminder = moment().add(formattedResponseTime.split(' ')[0], formattedResponseTime.split(' ')[1]).format()
+
+                    const timeout = {
+                        created: context.payload.issue.created_at,
+                        due: reminder
+                    }
+
+                    await metadata(context).set('timeout', timeout)
+                    await context.github.issues.addLabels(context.issue({ labels }))
                 }
-
-                const commentParams = context.issue({ body: commentMessage })
-
-                await context.github.issues.createComment(commentParams)
-
-                const { labels } = context.payload.issue
-                labels.push(config.reminder.label.waiting)
-                let reminder = moment().add(formattedResponseTime.split(' ')[0], formattedResponseTime.split(' ')[1]).format()
-
-                const timeout = {
-                    created: context.payload.issue.created_at,
-                    due: reminder
-                }
-
-                await metadata(context).set('timeout', timeout)
-                await context.github.issues.addLabels(context.issue({ labels }))
             }
         }
     }
